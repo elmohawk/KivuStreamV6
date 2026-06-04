ba41630cbc9f3376f2b3b7c5aeace6fd0bffdfe9
elmohawk <mrhawk2007@gmail.com>
2026-06-04 20:24:13 +0200

    Update watch.js

--- watch.js
+++ watch.js
@@ -2,7 +2,7 @@
    SUPABASE CONFIG
 ----------------------------*/
 const SUPABASE_URL = "https://exjgejujfxejjlbfizgz.supabase.co";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
 
 // Create client safely
 const supabaseClient = window.supabase
